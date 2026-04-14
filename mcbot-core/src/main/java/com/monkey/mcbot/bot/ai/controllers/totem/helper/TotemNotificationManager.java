d181f6f297bffb8b40d36c3f4396c00ce4c6e806
MonkeyMoon104 <lollolilo32@gmail.com>
2026-04-14 17:54:50 +0200

    Moving key

--- mcbot-core/src/main/java/com/monkey/mcbot/bot/ai/controllers/totem/helper/TotemNotificationManager.java
+++ mcbot-core/src/main/java/com/monkey/mcbot/bot/ai/controllers/totem/helper/TotemNotificationManager.java
@@ -18,7 +18,7 @@ public class TotemNotificationManager implements ITotemNotificationManager {
         var player = trainingBot.getTargetPlayer();
         if (player != null && player.isOnline()) {
             String msg = plugin.getConfig()
-                    .getString("bot.totem-finish", "[%botname%] Running out of totems");
+                    .getString("messages.totem-finish", "[%botname%] Running out of totems");
             String rawName = plugin.getConfig().getString("bot.name", "CrystalBot");
             String botName = rawName.replace("%player%", player.getName());
             msg = msg.replace("%botname%", botName);
