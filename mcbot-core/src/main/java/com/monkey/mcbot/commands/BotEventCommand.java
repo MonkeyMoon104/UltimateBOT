6a9a5d5e002784882285a303d26f6761ec5a7cfe
MonkeyMoon104 <lollolilo32@gmail.com>
2026-04-19 01:12:28 +0200

    commands: route /botevent gui opening through nms bridge

--- mcbot-core/src/main/java/com/monkey/mcbot/commands/BotEventCommand.java
+++ mcbot-core/src/main/java/com/monkey/mcbot/commands/BotEventCommand.java
@@ -2,7 +2,7 @@ package com.monkey.mcbot.commands;
 
 import com.monkey.mcbot.MinecraftBot;
 import com.monkey.mcbot.bot.BotType;
-import com.monkey.mcbot.gui.NewBotGUI;
+import com.monkey.mcbot.nms.NMSBridgeManager;
 import com.monkey.mcbot.utils.ChatColorUtils;
 import org.bukkit.World;
 import org.bukkit.command.Command;
@@ -45,7 +45,7 @@ public class BotEventCommand implements CommandExecutor {
             return true;
         }
 
-        new NewBotGUI(player, plugin, BotType.EVENT).open();
+        NMSBridgeManager.get().openBotGui(player, plugin, BotType.EVENT);
         return true;
     }
 
