8f0629a42b10ad5fc90e043f2e97e1e1ede27cda
MonkeyMoon104 <lollolilo32@gmail.com>
2026-04-19 01:12:34 +0200

    v26.1.1 gui items: add armor combat follow rank items

---
+++ versions/v26_1_1/src/main/java/com/monkey/mcbot/gui/v26_1_1/impl/CombatItem.java
@@ -0,0 +1,117 @@
+package com.monkey.mcbot.gui.v26_1_1.impl;
+
+import com.monkey.mcbot.MinecraftBot;
+import com.monkey.mcbot.bot.BotOptions;
+import com.monkey.mcbot.bot.BotType;
+import com.monkey.mcbot.bot.ai.rank.BotRank;
+import com.monkey.mcbot.utils.ChatColorUtils;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
+
+import org.jetbrains.annotations.NotNull;
+import xyz.xenondevs.invui.Click;
+import xyz.xenondevs.invui.item.ItemProvider;
+import xyz.xenondevs.invui.item.ItemBuilder;
+import xyz.xenondevs.invui.item.AbstractItem;
+
+import java.util.UUID;
+
+public class CombatItem extends AbstractItem {
+
+    private final MinecraftBot training;
+    private final BotOptions options;
+
+    public CombatItem(MinecraftBot training, BotOptions options) {
+        this.training = training;
+        this.options = options;
+    }
+
+    @Override
+    public ItemProvider getItemProvider(Player viewer) {
+        boolean status = options.isCombat();
+
+        ItemBuilder builder = new ItemBuilder(Material.valueOf(training.getLangString("gui.combat-button.material")));
+        builder.setLegacyName(ChatColorUtils.translate(training.getLangString("gui.combat-button.name")));
+        var loreLines = training.getLangStringList("gui.combat-button.lore");
+
+        for (String line : loreLines) {
+            String processedLine = line
+                    .replace("%type%", status ? "ON" : "OFF")
+                    .replace("%rank%", options.getRank().name());
+            builder.addLegacyLoreLines(ChatColorUtils.translate(processedLine));
+        }
+        return builder;
+    }
+
+    @Override
+    public void handleClick(@NotNull ClickType clickType, @NotNull Player player, @NotNull Click click) {
+        UUID managedOwnerUUID = resolveManagedOwnerUUID(player);
+
+        if (clickType.isLeftClick()) {
+            if (!options.isChangeableCombat()) {
+                String msg = training.getLangString("messages.combat-locked", "&cCombat is locked: it cannot be modified for this bot.");
+                player.sendMessage(ChatColorUtils.translate(msg));
+                return;
+            }
+
+            boolean oldStatus = options.isCombat();
+            boolean newStatus = !oldStatus;
+
+            if (newStatus && !options.isFollow()) {
+                String msg = training.getLangString("messages.combat-need-follow", "&cFollow must be ON to enable bot combat");
+                player.sendMessage(ChatColorUtils.translate(msg));
+                return;
+            }
+
+            options.setCombat(newStatus);
+            training.getBotManager().updateCombat(managedOwnerUUID, newStatus);
+
+            if (newStatus) {
+                training.getBotManager().switchBotToEnderpearl(managedOwnerUUID);
+                training.getServer().getScheduler().runTaskLater(training, () -> {
+                    training.getBotManager().switchBotToSword(managedOwnerUUID);
+                }, 1L);
+            } else {
+                if (training.getBotManager().getSwordSlot(managedOwnerUUID)) {
+                    int totemcount = options.getTotems();
+                    if (totemcount > 1) {
+                        training.getBotManager().switchBotToEnderpearl(managedOwnerUUID);
+                    } else {
+                        training.getBotManager().switchBotToEmpty(managedOwnerUUID);
+                    }
+                }
+            }
+
+            player.sendMessage(ChatColorUtils.translate("&eCombat " + (newStatus ? "&aON" : "&cOFF")));
+        }
+
+        if (clickType.isRightClick()) {
+            if (!options.isChangeableRank()) {
+                String msg = training.getLangString("messages.rank-locked", "&cRank is locked: it cannot be modified for this bot.");
+                player.sendMessage(ChatColorUtils.translate(msg));
+                return;
+            }
+
+            BotRank currentRank = options.getRank();
+            BotRank newRank = options.nextAllowedRank(currentRank, true);
+            options.setRank(newRank);
+
+            training.getBotManager().setBotRank(managedOwnerUUID, newRank);
+
+            player.sendMessage(ChatColorUtils.translate("&aRank set to &e" + newRank.name()));
+        }
+
+        notifyWindows();
+    }
+
+    private UUID resolveManagedOwnerUUID(Player player) {
+        if (options.getBotType() != BotType.TEAM_ALLY) {
+            return player.getUniqueId();
+        }
+        UUID teamOwnerUUID = training.getBotManager().findTeamAllyPrimaryOwner(player.getUniqueId());
+        return teamOwnerUUID == null ? player.getUniqueId() : teamOwnerUUID;
+    }
+}
+
+
---
+++ versions/v26_1_1/src/main/java/com/monkey/mcbot/gui/v26_1_1/impl/FollowItem.java
@@ -0,0 +1,96 @@
+package com.monkey.mcbot.gui.v26_1_1.impl;
+
+import com.monkey.mcbot.MinecraftBot;
+import com.monkey.mcbot.bot.BotOptions;
+import com.monkey.mcbot.bot.BotType;
+import com.monkey.mcbot.utils.ChatColorUtils;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
+
+import org.jetbrains.annotations.NotNull;
+import xyz.xenondevs.invui.Click;
+import xyz.xenondevs.invui.item.ItemProvider;
+import xyz.xenondevs.invui.item.ItemBuilder;
+import xyz.xenondevs.invui.item.AbstractItem;
+
+public class FollowItem extends AbstractItem {
+
+    private final MinecraftBot training;
+    private final BotOptions options;
+    private final CombatItem combatItem;
+
+    public FollowItem(MinecraftBot training, BotOptions options, CombatItem combatItem) {
+        this.training = training;
+        this.options = options;
+        this.combatItem = combatItem;
+    }
+
+    public FollowItem(MinecraftBot training, BotOptions options) {
+        this.training = training;
+        this.options = options;
+        this.combatItem = null;
+    }
+
+    @Override
+    public ItemProvider getItemProvider(Player viewer) {
+        boolean status = options.isFollow();
+
+        ItemBuilder builder = new ItemBuilder(Material.valueOf(training.getLangString("gui.follow-button.material")));
+        builder.setLegacyName(ChatColorUtils.translate(training.getLangString("gui.follow-button.name")));
+
+        var loreLines = training.getLangStringList("gui.follow-button.lore");
+
+        for (String line : loreLines) {
+            String processedLine = line.replace("%type%", status ? "ON" : "OFF");
+            builder.addLegacyLoreLines(ChatColorUtils.translate(processedLine));
+        }
+        return builder;
+    }
+
+    @Override
+    public void handleClick(@NotNull ClickType clickType, @NotNull Player player, @NotNull Click click) {
+        if (!options.isChangeableFollow()) {
+            String msg = training.getLangString("messages.follow-locked", "&cFollow is locked: it cannot be modified for this bot.");
+            player.sendMessage(ChatColorUtils.translate(msg));
+            return;
+        }
+
+        boolean oldFollowStatus = options.isFollow();
+        boolean combatStatus = options.isCombat();
+
+        boolean newFollowStatus = !oldFollowStatus;
+
+        if (!newFollowStatus && combatStatus && !options.isChangeableCombat()) {
+            String msg = training.getLangString("messages.follow-lock-combat", "&cYou cannot disable follow: combat is locked to ON.");
+            player.sendMessage(ChatColorUtils.translate(msg));
+            return;
+        }
+
+        options.setFollow(newFollowStatus);
+
+        var managedOwnerUUID = resolveManagedOwnerUUID(player);
+        training.getBotManager().updateFollow(managedOwnerUUID, newFollowStatus);
+
+        if (!newFollowStatus && combatStatus) {
+            options.setCombat(false);
+            training.getBotManager().updateCombat(managedOwnerUUID, false);
+
+            if (combatItem != null) {
+                combatItem.notifyWindows();
+            }
+        }
+
+        notifyWindows();
+    }
+
+    private java.util.UUID resolveManagedOwnerUUID(Player player) {
+        if (options.getBotType() != BotType.TEAM_ALLY) {
+            return player.getUniqueId();
+        }
+        java.util.UUID teamOwnerUUID = training.getBotManager().findTeamAllyPrimaryOwner(player.getUniqueId());
+        return teamOwnerUUID == null ? player.getUniqueId() : teamOwnerUUID;
+    }
+}
+
+
